678089a09fcdf8091f28c213e015f4727c21ccb3
Starry <80212489+lyc0929@users.noreply.github.com>
2024-04-16 10:43:51 +0800

    Add files via upload
    support multi gpu

--- run/ootd_train.py
+++ run/ootd_train.py
@@ -10,7 +10,6 @@ from torch.utils import data
 from torchvision import transforms
 import argparse
 from utils.dataloader import VITONDataset,VITONDataLoader,make_train_dataset
-from diffusers.utils.import_utils import is_xformers_available
 from safetensors.torch import save_file
 
 def get_opt():
@@ -23,7 +22,7 @@ def get_opt():
     parser.add_argument('--load_width', type=int, default=768)
     parser.add_argument('--shuffle', action='store_true')
 
-    parser.add_argument('--dataset_dir', type=str, default='/VTONDataset/VITON-HD/zalando-hd-resized/')
+    parser.add_argument('--dataset_dir', type=str, default='/mmu-vcg-ssd/yichen/VTONDataset/VITON-HD/zalando-hd-resized/')
     parser.add_argument('--dataset_mode', type=str, default='test')
     parser.add_argument('--dataset_list', type=str, default='test_pairs.txt')
     parser.add_argument('--checkpoint_dir', type=str, default='./checkpoints/')
@@ -134,6 +133,14 @@ def get_opt():
             " https://pytorch.org/docs/stable/generated/torch.optim.Optimizer.zero_grad.html"
         ),
     )
+    parser.add_argument(
+
+        "--conditioning_dropout_prob",
+        type = float,
+        default=0.1,
+        
+    )
+    
     # opt = parser.parse_args()
     opt, unknown = parser.parse_known_args()
     return opt
@@ -149,7 +156,7 @@ test_loader = VITONDataLoader(opt, test_dataset)
 # test_loader.data_loader.sampler = torch.utils.data.distributed.DistributedSampler(test_dataset)
 # torch.distributed.init_process_group(backend="nccl", rank=0, world_size=2)
 train_dataset = test_dataset
-train_dataloader = test_loader
+train_dataloader = test_loader.data_loader
 
 from transformers import CLIPImageProcessor, CLIPTextModel, CLIPTokenizer
 from diffusers.image_processor import PipelineImageInput, VaeImageProcessor
@@ -160,16 +167,16 @@ from diffusers import UniPCMultistepScheduler,PNDMScheduler
 from accelerate import Accelerator
 from accelerate.utils import ProjectConfiguration, set_seed
 import torch.nn.functional as F
-sys.path.append(r'/OOTDiffusion/ootd')
+sys.path.append(r'/mmu-vcg-ssd/yichen/OOTDiffusion/ootd')
 
 from pipelines_ootd.unet_vton_2d_condition import UNetVton2DConditionModel
 from pipelines_ootd.unet_garm_2d_condition import UNetGarm2DConditionModel
 
-VIT_PATH = "/OOTDiffusion/checkpoints/clip-vit-large-patch14"
-VAE_PATH = "/OOTDiffusion/checkpoints/ootd"
-UNET_PATH = "/OOTDiffusion/checkpoints/ootd/ootd_hd/checkpoint-36000/"
-MODEL_PATH = "/OOTDiffusion/checkpoints/ootd"
-scheduler_path = '/OOTDiffusion/checkpoints/ootd/scheduler/scheduler_ootd_config.json'
+VIT_PATH = "/mmu-vcg-ssd/yichen/OOTDiffusion/checkpoints/clip-vit-large-patch14"
+VAE_PATH = "/mmu-vcg-ssd/yichen/OOTDiffusion/checkpoints/ootd"
+UNET_PATH = "/mmu-vcg-ssd/yichen/OOTDiffusion/checkpoints/ootd/ootd_hd/checkpoint-36000/"
+MODEL_PATH = "/mmu-vcg-ssd/yichen/OOTDiffusion/checkpoints/ootd"
+scheduler_path = '/mmu-vcg-ssd/yichen/OOTDiffusion/checkpoints/ootd/scheduler/scheduler_ootd_config.json'
 
 vae = AutoencoderKL.from_pretrained(
             VAE_PATH,
@@ -191,24 +198,27 @@ unet_vton = UNetVton2DConditionModel.from_pretrained(
             use_safetensors=True,
         )
 ################
-new_in_channels = 8
-# Replace the first conv layer of the unet with a new one with the correct number of input channels
-conv_new = torch.nn.Conv2d(
-    in_channels=new_in_channels,
-    out_channels=unet_vton.conv_in.out_channels,
-    kernel_size=3,
-    padding=1,
-)
-
-torch.nn.init.kaiming_normal_(conv_new.weight)  # Initialize new conv layer
-conv_new.weight.data = conv_new.weight.data * 0.  # Zero-initialize new conv layer
-
-conv_new.weight.data[:, :4] = unet_vton.conv_in.weight.data  # Copy weights from old conv layer
-conv_new.bias.data = unet_vton.conv_in.bias.data  # Copy bias from old conv layer
-
-unet_vton.conv_in = conv_new  # replace conv layer in unet
-print('#######Replace the first conv layer of the unet with a new one with the correct number of input channels#######')
-# unet_garm.config['in_channels'] = new_in_channels  # update config
+if unet_vton.conv_in.in_channels == 4:
+    print("sxxxxxxxxxx")
+    with torch.no_grad():
+        new_in_channels = 8
+        # Replace the first conv layer of the unet with a new one with the correct number of input channels
+        conv_new = torch.nn.Conv2d(
+            in_channels=new_in_channels,
+            out_channels=unet_vton.conv_in.out_channels,
+            kernel_size=3,
+            padding=1,
+        )
+        
+        torch.nn.init.kaiming_normal_(conv_new.weight)  # Initialize new conv layer
+        conv_new.weight.data = conv_new.weight.data * 0.  # Zero-initialize new conv layer
+        
+        conv_new.weight.data[:, :4] = unet_vton.conv_in.weight.data  # Copy weights from old conv layer
+        conv_new.bias.data = unet_vton.conv_in.bias.data  # Copy bias from old conv layer
+        
+        unet_vton.conv_in = conv_new  # replace conv layer in unet
+        print('#######Replace the first conv layer of the unet with a new one with the correct number of input channels#######')
+        # unet_garm.config['in_channels'] = new_in_channels  # update config
 #################
 
 
@@ -254,9 +264,8 @@ accelerator = Accelerator(
     )
 
 optimizer_class = torch.optim.AdamW
+######单机单卡
 params_to_optimize = list(unet_garm.parameters()) + list(unet_vton.parameters())
-# params_to_optimize = list(unet_vton.parameters())
-
 optimizer = optimizer_class(
         params_to_optimize,
         lr=args.learning_rate,
@@ -264,10 +273,20 @@ optimizer = optimizer_class(
         weight_decay=args.adam_weight_decay,
         eps=args.adam_epsilon,
     )
+#######单机多卡
+# params_to_optimize = list(unet_garm.parameters())
+# optimizer = optimizer_class(
+#         params_to_optimize,
+#         lr=args.learning_rate,
+#         betas=(args.adam_beta1, args.adam_beta2),
+#         weight_decay=args.adam_weight_decay,
+#         eps=args.adam_epsilon,
+#     )
+# optimizer.add_param_group({'params': list(unet_vton.parameters())})
 
 # Scheduler and math around the number of training steps.
 overrode_max_train_steps = False
-num_update_steps_per_epoch = math.ceil(len(train_dataloader.data_loader) / args.gradient_accumulation_steps)
+num_update_steps_per_epoch = math.ceil(len(train_dataloader) / args.gradient_accumulation_steps)
 if args.max_train_steps is None:
     args.max_train_steps = args.num_train_epochs * num_update_steps_per_epoch
     overrode_max_train_steps = True
@@ -291,17 +310,25 @@ lr_scheduler = get_scheduler(
 #     accelerator.state.deepspeed_plugin.deepspeed_config_process(must_match=True, **kwargs)c
 
 
-# Prepare everything with our `accelerator`.
-# unet_garm,unet_vton, optimizer, train_dataloader, lr_scheduler, vae,image_encoder,text_encoder = accelerator.prepare(
-#         unet_garm,unet_vton, optimizer, train_dataloader, lr_scheduler,vae,image_encoder,text_encoder
-#     )
+#Prepare everything with our `accelerator`.
+class Unet_(torch.nn.Module):
+    def __init__(self, vton_model, garm_model):
+        super(Unet_, self).__init__()
+        self.unet_vton = vton_model
+        self.unet_garm = garm_model
+        # 其他初始化代码...
 
+unet_ = Unet_(unet_vton, unet_garm)
 
-
-
-unet_garm,unet_vton,optimizer, train_dataloader,test_loader, lr_scheduler = accelerator.prepare(
-         unet_garm,unet_vton,optimizer, train_dataloader, test_loader,lr_scheduler
+unet_ ,optimizer, train_dataloader, lr_scheduler = accelerator.prepare(
+        unet_, optimizer, train_dataloader, lr_scheduler
     )
+
+
+
+# unet_garm,unet_vton,optimizer, train_dataloader,test_loader, lr_scheduler = accelerator.prepare(
+#          unet_garm,unet_vton,optimizer, train_dataloader, test_loader,lr_scheduler
+#     )
 
 weight_dtype = torch.float32
 if accelerator.mixed_precision == "fp16":
@@ -320,17 +347,11 @@ text_encoder.to(accelerator.device, dtype=weight_dtype)
 # unet_garm.to(dtype=weight_dtype)
 # unet_vton.to(dtype=weight_dtype)
 
-##########   torch.compile  ###########
-# torch.compile(vae)
-# torch.compile(unet_garm)
-# torch.compile(unet_vton)
-# torch.compile(image_encoder)
-# torch.compile(text_encoder)
 
 
 
 # We need to recalculate our total training steps as the size of the training dataloader may have changed.
-num_update_steps_per_epoch = math.ceil(len(train_dataloader.data_loader) / args.gradient_accumulation_steps)
+num_update_steps_per_epoch = math.ceil(len(train_dataloader) / args.gradient_accumulation_steps)
 if overrode_max_train_steps:
     args.max_train_steps = args.num_train_epochs * num_update_steps_per_epoch
 # Afterwards we recalculate our number of training epochs
@@ -352,7 +373,7 @@ total_batch_size = args.train_batch_size * accelerator.num_processes * args.grad
 
 logger.info("***** Running training *****")
 logger.info(f"  Num examples = {len(train_dataset)}")
-logger.info(f"  Num batches each epoch = {len(train_dataloader.data_loader)}")
+logger.info(f"  Num batches each epoch = {len(train_dataloader)}")
 logger.info(f"  Num Epochs = {args.num_train_epochs}")
 logger.info(f"  Instantaneous batch size per device = {args.train_batch_size}")
 logger.info(f"  Total train batch size (w. parallel, distributed & accumulation) = {total_batch_size}")
@@ -363,18 +384,19 @@ first_epoch = 0
 
 initial_global_step = 0
 
-progress_bar = tqdm(
-        range(0, args.max_train_steps),
-        initial=initial_global_step,
-        desc="Steps",
-        # Only show the progress bar once on each machine.
-        disable=not accelerator.is_local_main_process,
-    )
+# progress_bar = tqdm(
+#         range(0, args.max_train_steps),
+#         initial=initial_global_step,
+#         desc="Steps",
+#         # Only show the progress bar once on each machine.
+#         disable=not accelerator.is_local_main_process,
+#     )
+
 image_logs = None
-for epoch in range(first_epoch, args.num_train_epochs):
-    for step, batch in enumerate(train_dataloader.data_loader):
+for epoch in tqdm(range(first_epoch, args.num_train_epochs)):
+    for step, batch in enumerate(train_dataloader):
         with accelerator.accumulate(unet_vton),accelerator.accumulate(unet_garm):
-        # with accelerator.accumulate(unet_garm):
+        # with accelerator.accumulate(unet_vton):
 
 
 
@@ -477,18 +499,36 @@ for epoch in range(first_epoch, args.num_train_epochs):
             # height, width = image_latents_vton.shape[-2:]
             # height = height * vae_scale_factor
             # width = width * vae_scale_factor
-            # import pdb;pdb.set_trace()
-            _, spatial_attn_outputs = unet_garm(
+            #####  modify: we should dropout the cloth condition! ##########      
+            if args.conditioning_dropout_prob is not None:
+                random_p = torch.rand(bsz, device=latents.device)
+                #########################################################
+
+                # Sample masks for the cloth images.
+                image_mask_dtype = image_latents_garm.dtype
+                image_mask = 1 - (
+                    (random_p >= args.conditioning_dropout_prob).to(image_mask_dtype)
+                    * (random_p < 3 * args.conditioning_dropout_prob).to(image_mask_dtype)
+                )
+                image_mask = image_mask.reshape(bsz, 1, 1, 1)
+                # Final image conditioning.
+                image_latents_garm = image_mask * image_latents_garm
+            ####################################################################
+
+            sample,spatial_attn_outputs = unet_garm(
             image_latents_garm,
             0,
             encoder_hidden_states=prompt_embeds,
             return_dict=False,)
-            
+
+
+            # unet_garm(image_latents_garm,0,encoder_hidden_states=prompt_embeds,return_dict=False,)
+            # import pdb;pdb.set_trace()
 
             latent_vton_model_input = torch.cat([noisy_latents, image_latents_vton], dim=1)
-            spatial_attn_inputs = spatial_attn_outputs.copy()
 
-            print('ootd')
+            spatial_attn_inputs = spatial_attn_outputs.copy()
+            
             noise_pred = unet_vton(
                     latent_vton_model_input,
                     spatial_attn_inputs,
@@ -497,24 +537,21 @@ for epoch in range(first_epoch, args.num_train_epochs):
                     return_dict=False,
                 )[0]
 
-            # import pdb;pdb.set_trace()
 
-            # if args.mixed_precision =='fp16':
-            #     loss = F.mse_loss(noise_pred.half(), noise, reduction="mean")
-            # else:
-            #     loss = F.mse_loss(noise_pred.float(), noise.float(), reduction="mean")
-            with accelerator.autocast():
-                loss = F.mse_loss(noise_pred.float(), noise.float(), reduction="mean")
-            print(loss)
+            # with accelerator.autocast():
+            util_adv_loss = torch.nn.functional.softplus(-sample).mean() * 0 
+            loss = F.mse_loss(noise_pred.float(), noise.float(), reduction="mean")+util_adv_loss
+            
+            print(loss.item())
 
             accelerator.backward(loss)
             optimizer.step()
             lr_scheduler.step()
             optimizer.zero_grad()
             
-            progress_bar.update(1)
+            # progress_bar.update(7)#####单机多卡，每次增加卡的数量
             accelerator.log({"training_loss": loss}, step=step)
-    if epoch%50 or sepoch == 0:
+    if (epoch%100 == 0 and epoch != 0 ) or epoch == (args.num_train_epochs-1):
         state_dict_unet_vton = unet_vton.state_dict()
         for key in state_dict_unet_vton.keys():
             state_dict_unet_vton[key] = state_dict_unet_vton[key].to('cpu')
@@ -526,7 +563,7 @@ for epoch in range(first_epoch, args.num_train_epochs):
         save_file(state_dict_unet_garm,f"./ootd_train_checkpoints/unet_garm-epoch{str(epoch)}.safetensors")
         # save_file(state_dict_unet_garm,f"./ootd_train_checkpoints/unet_garm.safetensors")                
         print('checkpoints successful saved')
-# accelerator.end_training()
+accelerator.end_training()
 # from safetensors.torch import save_file
 # save_file(unet_vton.to('cpu').state_dict(), "./unet_vton.safetensors")
 # save_file(unet_garm.to('cpu').state_dict(),"./unet_garm.safetensors")
